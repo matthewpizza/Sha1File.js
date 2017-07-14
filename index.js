8ab2ffa1cba143fd632598b771800f821bd9640b
Matthew Spencer <hello@thisismatthew.com>
2017-07-14 16:39:02 -0400

    File to sha1 hash string
    TODO:
    - Maybe Promise instead of cb
    - JS modules with simple UMD thing
    - Make cool for batches of files

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+var sjcl = require('./vendor/sjcl');
+
+/**
+ * @param  {File}     file
+ * @param  {Function} callback
+ */
+function Sha1File(file, callback) {
+  var read   = 0;
+  var step   = 1024 * 1024;
+  var h      = new sjcl.hash.sha1();
+  var reader = new FileReader();
+
+  /**
+   * @param  {ProgressEvent} evt
+   */
+  function onLoad(evt) {
+    var bits = arrayBufferToBitArray(evt.target.result);
+    h.update(bits);
+
+    read += step;
+
+    if (read < file.size) {
+      load();
+      return;
+    }
+
+    var hash = sjcl.codec.hex.fromBits(/*bitArray*/ h.finalize());
+
+    reader.removeEventListener('load', onLoad, false);
+    reader = null;
+    h = null;
+    callback && callback(hash);
+  }
+
+  /**
+   * @param  {ArrayBuffer} arrayBuffer
+   * @return {Array}       bits
+   */
+  function arrayBufferToBitArray(arrayBuffer) {
+    // Convert ArrayBuffer to Uint8Array.
+    // TODO: Cross browser testing
+    var bytes = new Uint8Array(arrayBuffer);
+
+    // Convert from an array of bytes to a bitArray.
+    var bits = sjcl.codec.bytes.toBits(bytes);
+
+    return bits;
+  }
+
+  function load() {
+    reader.readAsArrayBuffer(file.slice(read, read + step));
+  }
+
+  reader.addEventListener('load', onLoad, false);
+  load();
+}
+
+module.exports = Sha1File;
